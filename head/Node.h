1f534804954ac03bef1d7a3ccdede70a9d61cf07
UbeL13 <u.b.2004@yandex.ru>
2023-12-13 09:59:03 +0400

    Добавьте файлы проекта.

---
+++ head/Node.h
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <cstdlib>
+
+// Структура представления элемента списка
+template <typename T>
+struct Node {
+    T coefficient;
+    int degree;
+    Node<T>* next;
+
+    Node() : next(nullptr) {}
+    Node(const T& coeff, int deg) : coefficient(coeff), degree(deg), next(nullptr) {}
+};
+
+// Класс связного списка
+template <typename T>
+class LinkedList {
+private:
+    Node<T>* head;  // Указатель на начало списка
+    int size;       // Размер списка
+
+public:
+    LinkedList() : head(nullptr), size(0) {}
+    LinkedList(const LinkedList<T>& other) : head(nullptr), size(0) {
+        Node<T>* current = other.head;
+        while (current != nullptr) {
+            push_tail(current->coefficient, current->degree);
+            current = current->next;
+        }
+    }
+
+    
